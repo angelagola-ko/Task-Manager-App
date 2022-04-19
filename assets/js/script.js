cdacca255797d9097edfd426cf6baad2fb3eaa58
Angela Gola <95582357+angelaa1794@users.noreply.github.com>
2022-04-19 19:51:48 -0400

    Added code to tell what type it is- web, print, etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 
-var buttonE1 = document.querySelector("#save-task");
+//var buttonE1 = document.querySelector("#save-task");
+var formE1= document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a badabadoop";
+
+    //Create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info";
+    // add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+    //listItemE1.textContent = taskNameInput;
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
+//buttonE1.addEventListener("click", createTaskHandler);
 //buttonE1.addEventListener("click", function() {
 //    var listItemE1 = document.createElement("li");
 //    listItemE1.className = "task-item";
