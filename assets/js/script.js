0427e15add7fdda5ecab0c5712c328a0db3d7f2e
Angela Gola <95582357+angelaa1794@users.noreply.github.com>
2022-04-18 23:24:48 -0400

    simplified code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,18 @@
 var buttonE1 = document.querySelector("#save-task");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-buttonE1.addEventListener("click", function() {
+var createTaskHandler = function() {
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task";
+    listItemE1.textContent = "This is a badabadoop";
     tasksToDoE1.appendChild(listItemE1);
-});
+};
+
+buttonE1.addEventListener("click", createTaskHandler);
+//buttonE1.addEventListener("click", function() {
+//    var listItemE1 = document.createElement("li");
+//    listItemE1.className = "task-item";
+//    listItemE1.textContent = "This is a new task";
+//    tasksToDoE1.appendChild(listItemE1);
+//});
+
